587a030c642006028d6d283e2bfd861a70204222
magdump <pirjademl7@gmail.com>
2024-12-13 12:59:23 +0530

    chore: delete unwanted code as it has been extracted out in other modules

--- backend/pkg/websocket/websocket.go
+++ backend/pkg/websocket/websocket.go
@@ -1,22 +1,21 @@
 package websocket
 
 import (
-	"fmt"
-	"io"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
 )
 
-// upgrader function to upgrade from standard http connection to websocket longlasting connection
+// https://tutorialedge.net/projects/chat-system-in-go-and-react/part-1-initial-setup
+// upgrader function to upgrade from standard http connection to web Socket long-lasting connection
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
-func Upgrader(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
+func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
 	socket, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
@@ -25,49 +24,4 @@ func Upgrader(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
 
 	return socket, nil
 
-}
-
-func Reader(conn *websocket.Conn) {
-	for {
-		//read in message
-		msgType, msg, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		// no error can print the message
-		fmt.Println(string(msg))
-
-		if err := conn.WriteMessage(msgType, msg); err != nil {
-			log.Println(err)
-			return
-		}
-
-	}
-}
-
-func Writer(conn *websocket.Conn) {
-	for {
-		fmt.Println(("Sending"))
-		msgType, msg, err := conn.NextReader()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		w, err := conn.NextWriter(msgType)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		if _, err := io.Copy(w, r); err != nil {
-			fmt.Println(err)
-			return
-		}
-		if err := w.Close(); err != nil {
-			fmt.Println(err)
-			return
-		}
-
-	}
 }
