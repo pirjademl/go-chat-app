8dbed1d47f88be8aa0e04bba536594e4d383ab22
magdump <pirjademl7@gmail.com>
2024-12-13 12:51:44 +0530

    chore: add custom tailwind colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {},
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#f4f4f4",
+        secondary: "#ffffff",
+        textPrimary: "#333",
+        border: "#333",
+      },
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"],
+      },
+      borderRadius: {
+        xl: "calc(var(--radius) + 4px)",
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
