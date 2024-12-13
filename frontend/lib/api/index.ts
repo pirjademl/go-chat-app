84441e340ef3254419c8078fa40eeaaa5adacf67
magdump <pirjademl7@gmail.com>
2024-12-13 12:55:06 +0530

    chore :extract out sendMessage from connect method

--- frontend/lib/api/index.ts
+++ frontend/lib/api/index.ts
@@ -1,22 +1,20 @@
 var socket = new WebSocket("ws://localhost:8080/ws");
 
 let connect = (cb: any) => {
-    console.log("connecting");
-    socket.onopen = () => {
-        console.log("connected  to websocket");
-    };
-    socket.onmessage = (msg) => {
-        console.log(msg);
-        cb(msg);
-    };
-    socket.onclose = (event) => {
-        console.log("closing socket connection ", event);
-    };
-    socket.onerror = (err) => {
-        console.log("socket err", err);
-    };
+  socket.onopen = () => {
+    console.log("connected  to websocket");
+  };
+  socket.onmessage = (msg) => {
+    cb(msg);
+  };
+  socket.onclose = (event) => {
+    console.log("closing socket connection ", event);
+  };
+  socket.onerror = (err) => {
+    console.log("socket err", err);
+  };
 };
 let sendMessage = (msg: any) => {
-    socket.send(msg);
+  socket.send(msg);
 };
 export { connect, sendMessage };
